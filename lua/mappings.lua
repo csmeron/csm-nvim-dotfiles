5348e80f83649fc6d87edb5b6dd9b5365097a5da
csmeron <csmeron@proton.me>
2024-11-28 13:26:41 -0600

    thanksgiving commit with updated term binds

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,24 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- new terminals
+-- map("n", "<leader>h", function()
+--   require("nvchad.term").new { pos = "sp", cmd = "& $PROFILE" }
+-- end, { desc = "terminal new horizontal term" })
+--
+-- map("n", "<leader>v", function()
+--   require("nvchad.term").new { pos = "vsp", cmd = "& $PROFILE" }
+-- end, { desc = "terminal new vertical term" })
+--
+-- -- toggleable
+-- map({ "n", "t" }, "<A-v>", function()
+--   require("nvchad.term").toggle { pos = "vsp", cmd = "& $PROFILE", id = "vtoggleTerm" }
+-- end, { desc = "terminal toggleable vertical term" })
+--
+-- map({ "n", "t" }, "<A-h>", function()
+--   require("nvchad.term").toggle { pos = "sp", cmd = "& $PROFILE", id = "htoggleTerm" }
+-- end, { desc = "terminal toggleable horizonal term" })
+--
+-- map({ "n", "t" }, "<A-i>", function()
+--   require("nvchad.term").toggle { pos = "float", cmd = "& $PROFILE", id = "floatTerm" }
+-- end, { desc = "terminal toggleable floating term" })
